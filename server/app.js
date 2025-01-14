f9e777ba36a606722b49336775b7317758fb2f77
Marri5 <128259249+Marri5@users.noreply.github.com>
2025-01-14 10:16:35 +0100

    fix

--- server/app.js
+++ server/app.js
@@ -4,15 +4,11 @@ const employeeRoutes = require('./routes/employeeRoutes');
 
 const app = express();
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
 app.use(express.json());
 
-// Routes
 app.use('/api/employees', employeeRoutes);
 
-// Start Server
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
